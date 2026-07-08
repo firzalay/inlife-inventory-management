19d2cb5ba639e61623598979b52a50e64793c29f
firzalay <firza7004@gmail.com>
2026-07-08 12:07:26 +0700

    feat: change the default page to login authentication page

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/login');
 
 /**
  * Authenticated & verified dashboard route — accessible to Admin & Manager only.
